c28ccc56dbb741747e8a6289b17b64e6188f9559
OhTaeHo-97 <dhxogh123123@naver.com>
2023-06-12 16:28:15 +0900

    First Commit

---
+++ src/test/java/com/codestaes/TodoTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestaes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
